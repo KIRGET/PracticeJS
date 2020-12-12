ee4f8af8d7e61a70b22fc14cc91c33fa3a83fc95
KIRGET <kirilltopchienko@gmail.com>
2020-12-12 01:00:45 +0100

    my first console app

---
+++ js/sctipt.js
@@ -0,0 +1,18 @@
+const numberOfFilms = +prompt('how much films do you saw', '');
+const persanalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('last film do your watch', ''),
+    b = prompt('what rate it?', ''),
+    c = prompt('last film do your watch', ''),
+    d = prompt('what rate it?', '');
+
+persanalMovieDb[a] = b;
+persanalMovieDb[c] = d;
+
+console.log(persanalMovieDb);
